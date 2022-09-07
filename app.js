89be1346d947a5e288a2a2d75f058b41cbd53f2b
Katie Baeten <katherinebaeten@gmail.com>
2022-09-07 13:05:49 -0500

    basic functionality

---
+++ app.js
@@ -0,0 +1,12 @@
+const quoteButton = document.querySelector('js-new-quote');
+//this assigns a reference to the quote button to a NEW variable called newQuoteButton. Now we can use newQuoteButton forever in our js code
+
+
+//Use a DOM event signal to monitor when a user clicks on the page
+
+quoteButton.addEventListener('click', getQuote);
+
+function getQuote()
+{
+
+}
